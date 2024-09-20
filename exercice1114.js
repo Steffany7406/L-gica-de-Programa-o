528d67faf6e8868b77514c5119950c91c5d5f57e
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-20 19:45:34 -0300

    exercice1114.js

--- exercice1114.js
+++ exercice1114.js
@@ -1,9 +1,13 @@
-var num = parseInt(prompt("Digite sua senha:"));
-var pass = 2002; 
-while (pass !== num)
-if (pass === num){
-   document.write("Bem-vindo de volta!");  
-}else {
-    alert("Senha invalida!")
-}
    
+var num = parseInt(prompt("Digite sua senha:"));
+var pass = 2002;
+
+        while (true) {
+            if (num === pass) {
+                alert("Acesso Permitido");
+                break; // Para o loop quando a senha estiver correta
+            } else {
+                alert("Senha inválida!");
+                num = parseInt(prompt("Digite sua senha novamente:"));
+            }
+        }
