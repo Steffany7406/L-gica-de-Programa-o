1847ea549c197f5d493ff4b45280f3c1f4198d70
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-20 18:53:58 -0300

    Add files via upload

---
+++ exercice1114.js
@@ -0,0 +1,9 @@
+var num = parseInt(prompt("Digite sua senha:"));
+var pass = 2002; 
+while (pass !== num)
+if (pass === num){
+   document.write("Bem-vindo de volta!");  
+}else {
+    alert("Senha invalida!")
+}
+   
