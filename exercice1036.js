bd6b69f5d8b702cb13ff3f10ee510a42f8edd3b2
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-22 12:36:32 -0300

    exercice1036.js

--- exercice1036.js
+++ exercice1036.js
@@ -1,3 +1,5 @@
+// Math.sqrt = Retorna a raiz quadrada de um número
+// Math.pow = Retorna o valor de um número elevado a uma potencia especifica aceitando duas expressões base e expoente
 const A = parseFloat(prompt("Enter value for A: "));
 const B = parseFloat(prompt("Enter value for B: "));
 const C = parseFloat(prompt("Enter value for C: "));
@@ -7,9 +9,9 @@ let delta = Math.pow(B, 2) - 4 * A * C;
 if (delta < 0) {
   alert("Impossivel calcular");
 } else {
-  let root1 = (-B + Math.sqrt(delta)) / (2 * A);
-  let root2 = (-B - Math.sqrt(delta)) / (2 * A);
+  let count1 = (-B + Math.sqrt(delta)) / (2 * A);
+  let count2 = (-B - Math.sqrt(delta)) / (2 * A);
 
-  alert(`Raiz 1 = ${root1.toFixed(5)}`);
-  alert(`Raiz 2 = ${root2.toFixed(5)}`);
+  alert(`Raiz 1 = ${count1.toFixed(5)}`);
+  alert(`Raiz 2 = ${count2.toFixed(5)}`);
 }
