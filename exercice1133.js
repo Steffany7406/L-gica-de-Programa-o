e0b6a2f8a5261846e2c18c8c25b015400dc1a2f5
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-25 13:02:15 -0300

    Add files via upload

---
+++ exercice1133.js
@@ -0,0 +1,12 @@
+let uva = 10;
+let manga = 18;
+
+for (let i = uva; i <= manga; ++i){
+    if (i % 5 === 2){
+        console.log(`O resto é ${i}`);
+    }
+    if (i % 5 === 3){
+        console.log(`O resto é ${i}`); 
+    } 
+
+}
